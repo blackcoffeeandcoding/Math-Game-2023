18715f1f5a11351b1e5986ee9ad005308e9f6f75
natebermudez <natebermudez01@gmail.com>
2024-01-03 20:44:59 -0500

    Cleaned up code

--- Math Game 2023/Helpers.cs
+++ Math Game 2023/Helpers.cs
@@ -5,9 +5,24 @@ namespace Math_Game_2023
 {
     internal class Helpers
     {
-       internal static List<Game> games = new List<Game>();
+        internal static List<Game> games = new List<Game>
+        {
+        new Game { Date = DateTime.Now.AddDays(1), Type = GameType.Addition, Score = 5 },
+        new Game { Date = DateTime.Now.AddDays(2), Type = GameType.Multiplication, Score = 4 },
+        new Game { Date = DateTime.Now.AddDays(3), Type = GameType.Division, Score = 4 },
+        new Game { Date = DateTime.Now.AddDays(4), Type = GameType.Subtraction, Score = 3 },
+        new Game { Date = DateTime.Now.AddDays(5), Type = GameType.Addition, Score = 1 },
+        new Game { Date = DateTime.Now.AddDays(6), Type = GameType.Multiplication, Score = 2 },
+        new Game { Date = DateTime.Now.AddDays(7), Type = GameType.Division, Score = 3 },
+        new Game { Date = DateTime.Now.AddDays(8), Type = GameType.Subtraction, Score = 4 },
+        new Game { Date = DateTime.Now.AddDays(9), Type = GameType.Addition, Score = 4 },
+        new Game { Date = DateTime.Now.AddDays(10), Type = GameType.Multiplication, Score = 1 },
+        new Game { Date = DateTime.Now.AddDays(11), Type = GameType.Subtraction, Score = 0 },
+        new Game { Date = DateTime.Now.AddDays(12), Type = GameType.Division, Score = 2 },
+        new Game { Date = DateTime.Now.AddDays(13), Type = GameType.Subtraction, Score = 5 },
+        };
 
-        internal static void AddToHistory(int gameScore, GameType gameType)
+        internal static void AddToHistory(int gameScore,  GameType gameType)
         {
             games.Add(new Game
             {
@@ -40,6 +55,8 @@ namespace Math_Game_2023
 
        internal static void PrintGames() // This displays the game history stored in the games list 
         {
+            var gamesToPrint = games.Where(x => x.Date == new DateTime(2023, 12, 01)).OrderByDescending( x => x.Score);
+
             Console.Clear();
             Console.WriteLine("\r\n   _____                        _    _ _     _                   \r\n  / ____|                      | |  | (_)   | |                  \r\n | |  __  __ _ _ __ ___   ___  | |__| |_ ___| |_ ___  _ __ _   _ \r\n | | |_ |/ _` | '_ ` _ \\ / _ \\ |  __  | / __| __/ _ \\| '__| | | |\r\n | |__| | (_| | | | | | |  __/ | |  | | \\__ \\ || (_) | |  | |_| |\r\n  \\_____|\\__,_|_| |_| |_|\\___| |_|  |_|_|___/\\__\\___/|_|   \\__, |\r\n                                                            __/ |\r\n                                                           |___/ \r\n");
             Console.WriteLine("------------------------------------------------------------------------");
